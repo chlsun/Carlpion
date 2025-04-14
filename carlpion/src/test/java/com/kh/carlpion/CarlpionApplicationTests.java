bdc5252c31a72ea86e7a729aa2cad1ef61a259ca
chlsun <134248448+chlsun@users.noreply.github.com>
2025-04-14 10:57:44 +0900

    carlpion_setting

---
+++ carlpion/src/test/java/com/kh/carlpion/CarlpionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.carlpion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlpionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
