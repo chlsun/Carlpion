60b1f84c3ad54e4a0143c610089fb7d62d44e49a
chlsun <134248448+chlsun@users.noreply.github.com>
2025-04-14 10:57:44 +0900

    carlpion_setting

---
+++ carlpion/src/main/java/com/kh/carlpion/CarlpionApplication.java
@@ -0,0 +1,13 @@
+package com.kh.carlpion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlpionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlpionApplication.class, args);
+	}
+
+}
