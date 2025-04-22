974a3ea17cdc85ddf04366322f511bfdf91999b4
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-04-22 12:53:00 +0900

    new file delete

---
+++ carlpion/src/main/java/com/kh/carlpion/exception/exceptions/CreateDirectoriesException.java
@@ -0,0 +1,8 @@
+package com.kh.carlpion.exception.exceptions;
+
+public class CreateDirectoriesException extends RuntimeException {
+	
+	public CreateDirectoriesException(String message) {
+		super(message);
+	}
+}
