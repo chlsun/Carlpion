5ed7aa22b5d40d58ba2e20b32c5bca12570ea40f
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-04-23 17:29:54 +0900

    save

--- carlpion/src/main/java/com/kh/carlpion/security/SecurityConfigure.java
+++ carlpion/src/main/java/com/kh/carlpion/security/SecurityConfigure.java
@@ -40,8 +40,8 @@ public class SecurityConfigure {
 						   .cors(Customizer.withDefaults())
 						   .authorizeHttpRequests(request -> {
 							   request.requestMatchers("/admin/**").hasRole("ADMIN");
-							   request.requestMatchers(HttpMethod.GET, "/notice", "/notice/**", "/reports", "/reviews", "/uploads/**").permitAll();
-							   request.requestMatchers(HttpMethod.GET, "/reports/**", "/reviews/**").authenticated();
+							   request.requestMatchers(HttpMethod.GET, "/notice", "/notice/**", "/reports", "/reports/**", "/reviews", "/reviews/**").permitAll();
+							   request.requestMatchers(HttpMethod.GET, "/uploads/**").permitAll();
 							   request.requestMatchers(HttpMethod.POST, "/users", "/auth/**").permitAll();
 							   request.requestMatchers(HttpMethod.POST, "/rents", "/notice/**", "/reviews", "/reviews/**").authenticated();
 							   request.requestMatchers(HttpMethod.POST, "/notice", "/reports/**").hasRole("ADMIN");
