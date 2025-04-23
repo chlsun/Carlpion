7338d9d24e6ae432618ff56f260a0712300b729e
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-04-23 17:09:48 +0900

    Email Verify Processing

---
+++ carlpion/src/main/java/com/kh/carlpion/exception/exceptions/CustomMessagingException.java
@@ -0,0 +1,11 @@
+package com.kh.carlpion.exception.exceptions;
+
+public class CustomMessagingException extends RuntimeException {
+
+	private static final long serialVersionUID = 4048558136037067274L;
+
+	public CustomMessagingException(String msg) {
+		
+		super(msg);
+	}
+}
