9937abc89d1d96d5079a9a1a69a7470ec40a1467
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-04-24 17:49:48 +0900

    Send Email Process Done

--- carlpion/src/main/java/com/kh/carlpion/auth/model/dto/FindIdDTO.java
+++ carlpion/src/main/java/com/kh/carlpion/auth/model/dto/FindIdDTO.java
@@ -1,7 +1,6 @@
 package com.kh.carlpion.auth.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,15 +14,20 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class EmailDTO {
+public class FindIdDTO {
 
+	@Size(min = 2, max = 30, message = "이름 길이는 한글 2~5자, 영어 2~30자 사이여야 합니다.")
+	@Pattern(regexp = "^([a-zA-Z]{2,30}|[\\uAC00-\\uD7A3]{2,5})$", message = "잘못된 이름 형식입니다.")
+	@NotBlank(message = "이름은 비어있거나 공백이 포함될 수 없습니다.")
+	private String realname;
+	
 	@Size(min = 2, message = "이메일 길이는 2자 보다는 길어야 합니다.")
 	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "잘못된 이메일 형식입니다.")
 	@NotBlank(message = "이메일은 비어있거나 공백이 포함될 수 없습니다.")
 	private String email;
 	
-	@NotNull(message = "이메일 종류는 비어 있을 수 없습니다.")
-	private String type;
-	
+	@Size(min = 8, max = 8, message = "인증코드 길이는 8자 입니다.")
+	@Pattern(regexp = "^[A-Za-z0-9]{8}$", message = "잘못된 인증코드 형식입니다.")
+	@NotBlank(message = "인증코드는 비어있거나 공백이 포함될 수 없습니다.")
 	private String code;
 }
